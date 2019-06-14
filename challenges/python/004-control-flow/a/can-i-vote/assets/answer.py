c9ab535b4c87083005abfef39ab7675e00e06f03
kwajo <kwajom@gmail.com>
2019-06-14 13:35:36 +1000

    Update answer.py
    deleted blank lines

--- challenges/python/004-control-flow/a/can-i-vote/assets/answer.py
+++ challenges/python/004-control-flow/a/can-i-vote/assets/answer.py
@@ -1,4 +1,2 @@
-def can_i_vote(age):
-    return ("Hooray! You're old enough to have your say!" if age > 17 else "Whoops, you have to be at least 18 to vote.")
-    
-
+def can_i_vote(age): #I hope this counts as one line.
+    return ("Hooray! You're old enough to have your say!" if age > 17 else "Whoops, you have to be at least 18 to vote.")   
